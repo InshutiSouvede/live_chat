2ad1e1145f163a06a12ea7c09380a76b2f7f810d
Souvede Inshuti <isouvede2@gmail.com>
2024-02-24 08:35:47 +0200

    server setup and listening on the event to join room

---
+++ server/index.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const app = express();
+const http = require('http')
+const cors = require('cors');
+
+const {Server } =  require('socket.io')
+
+
+
+app.use(cors());
+
+const server = http.createServer(app);
+const io = new Server(server,{
+    cors:{
+        origin: "http://localhost:3000",
+        methods: ["GET","POST"],
+    },
+})
+
+// Initialize connection. Any socket.packeg that comes if not under "Connection", it will not be wellcome 
+io.on("connection", (socket)=>{// note that "connection" is all lower case
+    console.log("The socket id is", socket.id)
+    socket.on("join-room",(data)=>{
+        socket.join(data)
+        console.log(data.userName, "wants to join",data.roomId )
+    })
+    // When the user disconnects, print the id of that user
+    socket.on("Disconnection", ()=>{
+        console.log("The user", socket.id," just disconnected")
+    })
+})
+
+
+server.listen(4000, ()=>{
+    console.log("SERVER RUNNING ON 4000")
+}) 
